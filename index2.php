5cd72803ab4c5bec808cd4571d3766255ae4aa22
vlgkkkjsj <vic.vl674@gmail.com>
2024-03-14 11:28:57 -0300

    initial commit

---
+++ index2.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChatBoss</title>
+    <link rel="stylesheet" href="style2.css">
+    
+    <!-- boxicons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <div id="root">
+        <div class="container-root">
+            <header class="chatboss-header">
+                <div class="chatboss-header-est">
+                    <div class="chatboss-header-est-end">
+                        <div class="chatboss-header-est-img">
+                        </div>
+                        <div class="bnt-header-mid">
+                            <button class="bnt-home">
+                                <p class="bnt-home-sty">
+                                    Home
+                                </p>
+                            </button>
+                            <button class="bnt-sobre">
+                                <p class="bnt-sobre-sty">
+                                    Sobre
+                                </p>
+                            </button>
+                            <button class="bnt-planos">
+                                <p class="bnt-planos-sty">
+                                    Planos
+                                </p>
+                            </button>
+                            <button class="bnt-duvidas">
+                                <p class="bnt-duvidas-sty">
+                                    Dúvidas
+                                </p>
+                            </button>
+                        </div>
+                        <div class="bnt-header-log">
+                            <button class="bnt-header-login">
+                                Login
+                            </button>
+                        </div>
+                        <div class="bx bx-menu" id="menu-icon"></div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <section class="home-section">
+                    <div class="home-section-top">
+                        <button class="home-section-top-bnt">
+                            BlackConversa 2.0
+                        </button> 
+                        <div class="home-section-top-h1">
+                            <h1 class="home-section-top-h1-sty">
+                                A Ferramenta Mais Fácil Para Criar Chatbots Sem Código Do Brasil
+                            </h1>
+                        </div>
+                        <p class="home-section-top-p">
+                            A única empresa que empresta seu tempo de especialistas para te ajudar a deixar seu negócio no piloto automático 
+                            <br>
+                            sem te cobrar nada mais por isso!
+                        </p>
+                    </div>
+                    <div class="home-section-img">
+
+                    </div>
+                    <article class="home-section-mid">
+                        <div class="home-section-mid-top">
+                            <button class="home-section-mid-top-bnt">
+                                Inovação
+                                <span class="home-section-mid-top-bnt-span"></span>
+                            </button>
+                            
+                            <h2 class="home-section-mid-h1">Inovamos com a técnica de Transformação Boleto Gerado em Pix pago!</h2>
+                            
+                            <p class="home-section-mid-p">Sim, é possível transformar seus boletos gerados em pix pago. Imagine a seguinte situação, o cliente gerou o boleto, e esqueceu de pagar ou desistiu da compra, isso faz com que você perca 7 em 10 vendas. Mas com o BlackConversa a história é outra, seu cliente gerou um boleto e na mesma hora nosso chatbot entra em contato com seu cliente no WhatsApp transformando seu boleto NÃO PAGO em PIX PAGO e o melhor de maneira 100% automatizada e humanizada, 7 em cada
+                                <strong> 10 boletos serão convertidos em Pix pagos.</strong>
+                            </p>
+
+                        </div>
+
+                        <div class="home-section-mid-img">
+
+                        </div>
+                    </article>
+                    <div class="home-section-bot">
+                        <article class="home-section-bot-mid-box">
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                            <div class="home-section-mid-box-minibox">
+                                <div class="home-section-mid-box-minibox-sty">
+                                    <h3 class="home-section-mid-box-minibox-sty-h3">Tenha atendentes ilimitados em um único número.</h3>
+                                    <p class="home-section-mid-box-minibox-sty-p">Economize tempo e dinheiro com sua equipe em um único número, praticidade e economia</p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+            </main>
+
+
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
